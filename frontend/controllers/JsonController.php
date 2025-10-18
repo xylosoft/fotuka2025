6b0dc6041908fb2934982868266326b51f81b1a5
xylosoft <orozco_rafael@yahoo.com>
2025-10-18 12:21:11 -0600

    Only display folders owned by the company

--- frontend/controllers/JsonController.php
+++ frontend/controllers/JsonController.php
@@ -11,11 +11,11 @@ class JsonController extends Controller
     public function actionFolders()
     {
         \Yii::$app->response->format = Response::FORMAT_JSON;
-
         // Recursive function to build folder hierarchy
         $buildTree = function ($parentId = null) use (&$buildTree) {
+            $user = \Yii::$app->user->identity;
             $folders = Folder::find()
-                ->where(['parent_id' => $parentId, 'status' => Folder::STATUS_ACTIVE])
+                ->where(['parent_id' => $parentId, 'status' => Folder::STATUS_ACTIVE, 'customer_id' => $user->customer_id])
                 ->orderBy(['name' => SORT_ASC])
                 ->all();
 

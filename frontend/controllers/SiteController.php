c731d82829fbbe470b7b37f01b789fbf9671ac50
xylosoft <orozco_rafael@yahoo.com>
2026-02-27 14:16:54 -0600

    Reset Password changes

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -181,7 +181,7 @@ class SiteController extends Controller
             if ($model->sendEmail()) {
                 Yii::$app->session->setFlash('success', 'Check your email for further instructions.');
 
-                return $this->goHome();
+                return $this->redirect(['site/login']);
             }
 
             Yii::$app->session->setFlash('error', 'Sorry, we are unable to reset password for the provided email address.');
